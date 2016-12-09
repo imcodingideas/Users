d1dbfe8de06d0eee3c4ce95858a01f305e9dead5
Joseph Chambers <joseph@michael-chambers.com>
2016-12-09 15:54:35 -0700

    Told mongoose to use the global es6 Promise

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => console.log('good to go'))
-    .on('error', (error) => {
-        console.warn('Warning ', error);
+mongoose.Promise = global.Promise;
+
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn('Warning ', error);
+        });
 });
 
 beforeEach((done) => {
