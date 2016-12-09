e54dc664ca30f9acdb5f0ecb3ce5cba67bded47e
Joseph Chambers <joseph@michael-chambers.com>
2016-12-09 16:32:34 -0700

    automated tests with nodemon. Found particular records with find and findOne methods

--- test/reading_test.js
+++ test/reading_test.js
@@ -4,13 +4,25 @@ const User = require('../src/user');
 describe('Reading users out of the database', () => {
     let joe; // instances of a user
 
-    beforeEach(() => {
+    beforeEach((done) => {
         joe = new User({name: 'Joe'});
         joe.save()
             .then(() => done());
     });
 
-    it('Finds all users with the name of Joe', () => {
-
+    it('Finds all users with the name of Joe', (done) => {
+        User.find({name: 'Joe'})
+            .then((users) => {
+                assert(users[0]._id.toString() === joe._id.toString());
+                done();
+            });
     });
+
+    it('Find a user with a particular id', (done) => {
+        User.findOne({ _id: joe._id })
+            .then((user) => {
+                assert(user.name === 'Joe');
+                done();
+            });
+    })
 });
