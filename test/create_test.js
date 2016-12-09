9d6f12b6e663d9992805e86b1199727a548fa790
Joseph Chambers <joseph@michael-chambers.com>
2016-12-09 15:46:35 -0700

    used mongoose isNew() property to return truthly value to test assert

--- test/create_test.js
+++ test/create_test.js
@@ -2,9 +2,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating Records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         const joe = new User({ name: 'Joe' });
 
-        joe.save();
+        joe.save()
+            .then(() => {
+                //has joe been saved to the database?
+                assert(!joe.isNew);
+                done();
+            });
     });
 });
