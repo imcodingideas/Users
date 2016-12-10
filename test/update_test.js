63e0d9f57d9f941d6eb58ceeb0a105c9536bde94
Joseph Chambers <joseph@michael-chambers.com>
2016-12-09 22:03:31 -0700

    created the model based update tests

--- test/update_test.js
+++ test/update_test.js
@@ -29,4 +29,25 @@ describe('Updating a user', () => {
         assertName(joe.update({ name: 'Alex'}), done);
     });
 
+    it('A model class can update', (done) => {
+        assertName(
+            User.update({ name: 'Joe' }, { name: 'Alex'}),
+            done
+        );
+    });
+
+    it('A model class can update one record', (done) => {
+        assertName(
+        User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }),
+            done
+        )
+    });
+
+    it('A model class can find a record with an ID and update', (done) => {
+        assertName(
+            User.findByIdAndUpdate( joe._id, { name: 'Alex'}),
+            done
+        );
+    });
+
 });
