573972d1b74f05a3662980401666e5b1e9d1231b
Joseph Chambers <joseph@michael-chambers.com>
2016-12-12 21:28:19 -0700

    added postCount to user model, and wrote an update test using mongoose operators

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating a user', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', postCount: 0 });
         joe.save()
             .then(() => done());
     });
@@ -48,6 +48,15 @@ describe('Updating a user', () => {
             User.findByIdAndUpdate( joe._id, { name: 'Alex'}),
             done
         );
+    });
+
+    it('A user can have their post count incremented by 10', (done) => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 10 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then((user) => {
+                assert(user.postCount === 10);
+                done();
+            });
     });
 
 });
