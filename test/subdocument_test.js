faa4da4d7a2c4db1d8f24b6ddde357d2d825ef0f
Joseph Chambers <joseph@michael-chambers.com>
2017-01-08 14:50:24 -0700

    adding subdocuments to existing records

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -2,17 +2,37 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Subdocuments', () => {
-   it('can create subdocument', (done) => {
-      const joe = new User({
-          name: 'Joe',
-          posts: [{ title: 'Hello World' }]
+  it('can create subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{title: 'Hello World'}]
+    });
+
+    joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts[0].title === 'Hello World');
+        done();
+      });
+  });
+
+  it('Can add subdocuments to an existing record', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        user.posts.push({title: 'New Post'});
+        return user.save();
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts[0].title === 'New Post');
+        done();
       });
 
-      joe.save()
-          .then(() => User.findOne({ name: 'Joe' }))
-          .then((user) => {
-            assert(user.posts[0].title === 'Hello World');
-            done();
-          });
-   });
+  });
 });
