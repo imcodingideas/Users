1d58f054081f568441c969e2404c8d7b7d108555
Joseph Chambers <joseph@michael-chambers.com>
2017-01-08 15:02:56 -0700

    removing subdocuments

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -35,4 +35,24 @@ describe('Subdocuments', () => {
       });
 
   });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{title: 'New Title'}]
+    });
+
+    joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        const post = user.posts[0];
+        post.remove();
+        return user.save();
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+  });
 });
